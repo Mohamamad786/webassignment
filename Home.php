05150f86e845bb533e12770a1e11234b5055b1fd
unknown <100490152@UOSL15-R90266NN.oncampus.local>
2016-02-03 18:18:08 -0500

    intitial commit

---
+++ Home.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+include_once 'dbconnect.php';
+
+if(!isset($_SESSION['user']))
+{
+ header("Location: index.php");
+}
+$res=mysql_query("SELECT * FROM users WHERE user_id=".$_SESSION['user']);
+$userRow=mysql_fetch_array($res);
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Welcome - <?php echo $userRow['email']; ?></title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<div id="header">
+ <div id="left">
+    <label>Welcome To Mohammad's Kingdom</label>
+    </div>
+    <div id="right">
+     <div id="content">
+         hi' <?php echo $userRow['username']; ?>&nbsp;<a href="logout.php?logout">Sign Out</a>
+        </div>
+        
+    </div>
+</div>
+</body>
+    <body>
+        <div id ="middle">
+            </div id ="left">
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+    </body> 
+</html>
