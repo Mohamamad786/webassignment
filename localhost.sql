875afd3f52c24e88844a999d631002614144179e
unknown <100490152@UOSL15-R90266NN.oncampus.local>
2016-02-04 11:38:11 -0500

    intitial commit

---
+++ localhost.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 04, 2016 at 04:35 PM
+-- Server version: 5.5.8
+-- PHP Version: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `dbtest`
+--
+CREATE DATABASE `dbtest` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `dbtest`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(5) NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) NOT NULL,
+  `email` varchar(35) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`) VALUES
+(1, 'mohammad', 'm@yahoo.com', '40a810872e9d60c222157f9f810c204d'),
+(2, 'mohammad', 'G96161@YAHOO.COM', '6b4e69f1a78526824c03e597a7824270'),
+(3, 'fuad', 'mohammadfarazeducation@yahoo.com', '6b4e69f1a78526824c03e597a7824270');
