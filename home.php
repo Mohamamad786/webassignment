e0abd0748f915d71dd36325e822fbcfcad4ebe3e
unknown <100490152@UOSL15-R90266NN.oncampus.local>
2016-02-09 23:36:31 -0500

    intitial commit

--- home.php
+++ home.php
@@ -38,4 +38,33 @@ $userRow=mysql_fetch_array($res);
     <input type="submit" value="Upload Image" name="submit">
 </form>
     </body> 
-</html>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <div id="content">
+    Hey!! try out the map!!
+    </div>
+    
+<head>
+<script src="http://maps.googleapis.com/maps/api/js"></script>
+<script>
+function initialize() {
+  var mapProp = {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+</script>
+</head>
+
+<body>
+<div id="googleMap" style="width:500px;height:380px;"></div>
+</body>
+
+</html> 
+
+
+ 
+    
