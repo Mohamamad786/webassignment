2129c381a286d81a42ab709ed0a3afa7f31c0f03
unknown <100490152@UOSL15-R90266NN.oncampus.local>
2016-02-10 00:37:07 -0500

    intitial commit

--- upload.php
+++ upload.php
@@ -1,5 +1,22 @@
+
+
+
 <?php
 
+include 'vendor/autoload.php';
+use Aws\Resource\Aws;
+
+use Aws\S3\S3Client;
+
+// Instantiate an Amazon S3 client.
+$s3 = new S3Client([
+    'version' => 'latest', 
+    'region' => 'us-west-2', 
+    'http' => ['verify' => false], 
+    'credentials' => array(
+        'key' => 'AKIAJ4VDTJN7TF3MROUA', 
+        'secret' => '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'
+)]);
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -37,6 +54,14 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) { 
+        try {// Storing user images on a AWS S3 for permanent storage.
+            $s3 -> putObject(['Bucket' => 'mohammad786', 
+            'Key' => 'uploads/' . basename($_FILES["fileToUpload"]["name"]), 
+            'Body' => fopen($target_file, 'r'), 'ACL' => 'public-read', ]);
+        } catch (Aws\Exception\S3Exception $e) {
+            echo "There was an error uploading the file to S3.\n";
+            die();
+        }
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
